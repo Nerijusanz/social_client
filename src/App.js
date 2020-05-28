a9a867fbd24a247407f00250af262269c4274bd1
nerijus-ubuntu <nerijusanz@gmail.com>
2020-05-28 08:52:08 +0300

    Installed react-router-dom, Added Pages, Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <h2>Social APi</h2>
-    </div>
-  );
+import Home from './pages/Home';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/login" exact component={Login} />
+            <Route path="/signup" exact component={Signup} />
+          </switch>
+        </Router>
+      </div>
+    )
+  }
 }
-
-export default App;
