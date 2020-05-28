1e0b5a1b57a62d150f53e709d4e68781a1e2d007
nerijus-ubuntu <nerijusanz@gmail.com>
2020-05-28 16:25:00 +0300

    Scream.js screamList updated

---
+++ src/components/pages/Dashboard/Screams/ScreamItem.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ScreamItem({scream}) {
+  return (
+    <p>{scream.body}</p>
+  )
+}
--- src/components/pages/Dashboard/Screams/Screams.js
+++ src/components/pages/Dashboard/Screams/Screams.js
@@ -1,11 +1,63 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
+import Grid from '@material-ui/core/Grid';
+
+import ScreamItem from './ScreamItem';
+
+ 
 
 export default class Screams extends Component {
+
+  state={
+    screams:null,
+    loading:false
+  }
+
+  componentDidMount(){
+    
+    this.getScreams();
+  }
+
+  getScreams = async () => {
+
+    const API = 'https://europe-west1-socialapi-c2ee5.cloudfunctions.net/api';
+
+    try{
+      this.setState({loading:true});
+      const screams = await axios.get(API+'/screams');
+
+      if(! await screams.data) return;
+      
+      this.setState({screams: screams.data});
+
+      console.log(this.state.screams);
+
+      this.setState({loading:false});
+      
+    }catch(err){
+      this.setState({loading:false});
+      console.log(err)
+    }
+
+
+  }
+
+
   render() {
+
+    const {loading,screams} = this.state;
+
+    
+    let screamsList = !screams ? (<p>no screams added yet. Add scream here</p>):( screams.map(scream => <ScreamItem key={scream.screamId} scream={scream} />))
+
+
     return (
-      <div>
-        Screams
-      </div>
+      <Grid container spacing={2}>
+        <Grid item sm={8} xs={12}>
+          {loading ? (<p>Loading...</p>):null}
+          {!loading && screamsList}
+        </Grid>
+      </Grid>
     )
   }
 }
