3dccd8f5f11f799c959d10c1ebc95089814c15f0
nerijus-ubuntu <nerijusanz@gmail.com>
2020-05-28 16:23:38 +0300

    Home.js updated. Clean content

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,47 +1,16 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import Grid from '@material-ui/core/Grid';
-import createMixins from '@material-ui/core/styles/createMixins';
 
-//api url: https://europe-west1-socialapi-c2ee5.cloudfunctions.net/api 
 
 export default class home extends Component {
-
-  state={
-    screams:null
-  }
-
-  componentDidMount(){
-    
-    this.getScreams();
-  }
-
-  getScreams = async () => {
-
-    const API = 'https://europe-west1-socialapi-c2ee5.cloudfunctions.net/api';
-
-    try{
-      const screams = await axios.get(API+'/screams');
-
-      this.setState({screams: screams.data});
-      
-      console.log(this.state.screams);
-      
-    }catch(err){console.log(err)}
-
-
-  }
 
 
   render() {
 
-    let screamsList = !this.state.screams ? (<p>no screams</p>):( this.state.screams.map(scream => <p key={scream.screamId}>{scream.body}</p>))
-
-
     return (
       <Grid container spacing={2}>
         <Grid item sm={8} xs={12}>
-          {screamsList}
+          Home Page
         </Grid>
         <Grid item sm={4} xs={12}>
           <p>Profile...</p>
