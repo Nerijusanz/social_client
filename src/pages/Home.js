d7463644999764789a83248e9774ca062945cf93
nerijus-ubuntu <nerijusanz@gmail.com>
2020-05-28 13:09:57 +0300

    Home.js update. Grid space c

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,48 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
+import Grid from '@material-ui/core/Grid';
+
+//api url: https://europe-west1-socialapi-c2ee5.cloudfunctions.net/api 
 
 export default class home extends Component {
+
+  state={
+    screams:null
+  }
+
+  
+
+  componentDidMount(){
+    
+    this.getScreams();
+  }
+
+  getScreams = async () => {
+
+    const API = 'https://europe-west1-socialapi-c2ee5.cloudfunctions.net/api';
+
+    try{
+      const screams = await axios.get(API+'/screams');
+
+      this.setState({screams: screams.data});
+  
+      console.log(this.state.screams);
+    }catch(err){console.log(err)}
+
+
+  }
+
+
   render() {
     return (
-      <div>
-        Home page
-      </div>
+      <Grid container spacing={2}>
+        <Grid item sm={8} xs={12}>
+          <p>Content...</p>
+        </Grid>
+        <Grid item sm={4} xs={12}>
+          <p>Profile...</p>
+        </Grid>
+      </Grid>
     )
   }
 }
