0258a90dffd44519e08128b442820652859e04cf
nerijus-ubuntu <nerijusanz@gmail.com>
2020-05-28 11:33:44 +0300

    App.js updated

--- src/App.js
+++ src/App.js
@@ -12,28 +12,11 @@ import Signup from './pages/Signup';
 import Login from './pages/Login';
 
 
-const theme = createMuiTheme({
-  palette:{
-    primary:{
-      light: '#33c9dc',
-      main:'#00bcd4',
-      dark:'#008394',
-      contrastText:'#fff'
-    },
-    secondary:{
-      light:'#ff6333',
-      main:'#ff3d00',
-      dark:'#b22a00',
-      contrastText:'#fff'
-    }
-  }
-});
-
 export default class App extends Component {
   render() {
     return (
 
-      <MuiThemeProvider theme={theme}>
+      <MuiThemeProvider theme={cssTheme}>
         <div className="App">       
           <Router>
             <Navbar/>
@@ -51,3 +34,20 @@ export default class App extends Component {
     )
   }
 }
+
+const cssTheme = createMuiTheme({
+  palette:{
+    primary:{
+      light: '#33c9dc',
+      main:'#00bcd4',
+      dark:'#008394',
+      contrastText:'#fff'
+    },
+    secondary:{
+      light:'#ff6333',
+      main:'#ff3d00',
+      dark:'#b22a00',
+      contrastText:'#fff'
+    }
+  }
+});
