9dedc241e70ba77dbf2e532af5199bf52300bd46
nerijus-ubuntu <nerijusanz@gmail.com>
2020-05-29 13:23:32 +0300

    ScreamItem.js update. format date by day.js()

--- src/components/pages/Dashboard/Screams/ScreamItem.js
+++ src/components/pages/Dashboard/Screams/ScreamItem.js
@@ -7,18 +7,28 @@ import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+
+
 
 
 export default function SimpleItem({scream}) {
   const classes = useStyles();
 
+  //----------date format-----------------
+  dayjs.extend(relativeTime);
+  const dateAgo = dayjs().from(dayjs(scream.createdAt));
+
+  //--------------------------------------
+  
 
   return (
     <Card className={classes.root}>
       <CardContent>
         <Typography variant="h5" component={Link} to={`users/${scream.userHandle}`}>{scream.userHandle}</Typography>
         <Typography variant="body1" component="p">{scream.body}</Typography>
-        <Typography variant="body2" component="p">{scream.createdAt}</Typography>
+        <Typography variant="body2" component="p">{dateAgo}</Typography>
       </CardContent>
       <CardActions>
         <Button size="small">Learn More</Button>
