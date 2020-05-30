fb55d861b7d798fd1230d78286b9154cacad1a56
nerijus-ubuntu <nerijusanz@gmail.com>
2020-05-30 11:06:16 +0300

    styles/theme.js created. Added to App.js theme

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 
 import './App.css';
+import themeStyles from './components/styles/theme.js';
 
 import Navbar from './components/navigation/Navbar';
 import Home from './components/pages/Home/Home';
@@ -17,7 +18,7 @@ export default class App extends Component {
   render() {
     return (
 
-      <MuiThemeProvider theme={cssTheme}>
+      <MuiThemeProvider theme={createMuiTheme(themeStyles)}>
         <div className="App">       
           <Router>
             <Navbar/>
@@ -36,23 +37,3 @@ export default class App extends Component {
     )
   }
 }
-
-const cssTheme = createMuiTheme({
-  palette:{
-    primary:{
-      light: '#33c9dc',
-      main:'#00bcd4',
-      dark:'#008394',
-      contrastText:'#fff'
-    },
-    secondary:{
-      light:'#ff6333',
-      main:'#ff3d00',
-      dark:'#b22a00',
-      contrastText:'#fff'
-    }
-  },typography:{
-    useNextVariants: true
-  }
-
-});
