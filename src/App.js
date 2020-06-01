99592d8dea14c1e56775e1a8aabe3c064d1799c8
nerijus-ubuntu <nerijusanz@gmail.com>
2020-06-01 17:56:57 +0300

    authenticated routes created

--- src/App.js
+++ src/App.js
@@ -1,40 +1,138 @@
 import React, { Component } from 'react';
-import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
+import {Route,Switch,Redirect} from 'react-router-dom';
+import Loader from 'react-loader';
+//import propTypes from 'prop-types';
+import jwtDecode from 'jwt-decode';
 
 import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+
 
 import Navbar from './components/navigation/Navbar';
 import Home from './components/pages/Home/Home';
 import Signup from './components/pages/Auth/Signup/Signup';
 import Login from './components/pages/Auth/Login/Login';
+
+// --------------Auth Routes-------------------------------------
+import GuestRoute from './components/routes/GuestRoute';
+import AuthRoute from './components/routes/AuthRoute';
+// ---------------------------------------------------------
+
+
 import Screams from './components/pages/Dashboard/Screams/Screams';
 
 import './App.css';
 import theme from './components/styles/theme.js';
-const MuiTheme = createMuiTheme(theme)
+const MuiTheme = createMuiTheme(theme);
+
+
+
+
+// -----------------end Pages----------------------------------------
+
+
+const defaultRoute = () => <Redirect to="/" />
+
+
+
+
 
 
 export default class App extends Component {
+
+  state={
+    auth:{
+      isAuthenticated:false,
+      loading:false,
+    }
+  }
+
+  componentDidMount(){
+
+    this.initApp();
+
+  }
+
+  initApp = () =>{
+
+    this.authentication();
+
+  };
+
+  authentication = async () => {
+
+    this.setState({
+      auth:{
+          ...this.state.auth,
+          loading:true
+      }
+    });
+
+    const token = localStorage.SC_token;
+    let isAuthenticated=false
+    if(token){
+      const decodedToken =  await jwtDecode(token);
+      //console.log(decodedToken);
+
+      if(decodedToken.exp * 1000 > Date.now() )
+        isAuthenticated=true;
+
+    }
+
+    this.setState({
+      auth:{
+          ...this.state.auth,
+          isAuthenticated,
+          loading:false
+      }
+    });
+
+  };
+
+
+
   render() {
+
+    const {loading,isAuthenticated} = this.state.auth;
+    /*
+    //test authentication
+    const loading=false;
+    const isAuthenticated=false;
+    */
+      
+
     return (
 
-      <MuiThemeProvider theme={MuiTheme}>
-        <div className="App">       
-          <Router>
-            <Navbar/>
-            <div className="container">
+      <Loader loaded={!loading}>
+
+        <MuiThemeProvider theme={MuiTheme}>
+          <div className="App">       
             
-            <Switch>
-              <Route path="/" exact component={Home} />
-              <Route path="/login" exact component={Login} />
-              <Route path="/signup" exact component={Signup} />
-              <Route path="/screams" exact component={Screams} />
-            </Switch>
-            </div>
-          </Router>
-        </div>
-      </MuiThemeProvider>
+          
+            !loading ? 
+            <>
+              <Navbar isAuthenticated={isAuthenticated}/>
+              <div className="container">
+            
+                <Switch>
+                  <Route path="/" exact component={Home} />    
+                  
+                  <GuestRoute isAuthenticated={isAuthenticated} path="/signup" exact component={Signup} />
+                  <GuestRoute isAuthenticated={isAuthenticated} path="/login" exact component={Login} />
+
+                  <AuthRoute isAuthenticated={isAuthenticated} path="/screams" exact component={Screams} />
+                  
+                  <Route component={defaultRoute} />
+
+                </Switch>
+              </div>
+            </>             
+          
+          </div>
+        </MuiThemeProvider>
+
+      </Loader>
     )
   }
 }
+
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router,Route} from 'react-router-dom';
 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Route component={App}></Route>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
